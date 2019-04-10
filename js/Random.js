62e9762af40674947d41f447e2ac391c2325380c
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-10 15:24:29 -0500

    Finished Array intro exercise

---
+++ js/Random.js
@@ -0,0 +1,44 @@
+// Using a while loop:
+// - log all even numbers from 1 - 100
+// – create a function that returns the square of a number then return all squares from 1 -10
+// var i =1;
+// while(i<=100){
+//     if (i%2===0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+//
+// function square(input){
+//     return input*input;
+// }
+//
+// var x=1;
+// while(x<=10){
+//     console.log(square(x));
+//     x++;
+// }
+//
+//
+// Using a do-while loop:
+// –    log “The number is <i>” for numbers 1-25
+// –    subtract by 10 from 100 until you reach -50.
+// var i=0;
+// do{
+//     i++;
+//     console.log("The number is " + i);
+// }while(i<25);
+// i=100;
+// do{
+//     console.log(i);
+//     i -=10;
+// }while(i>-50);
+
+// Fizz Buzz: log all numbers from 1 - 100
+// –    if divisible by 3 log “Fizz"
+// –    if divisible by 5 log “Buzz"
+// –    if divisible by 3 and 5 log “FizzBuzz”
+// –    else, log the current number
+// for(var i=1;i<100;i++){console.log((i%3?"":"Fizz")+(i%5?"":"Buzz")||i)}
+// for(var i=0;i<100;)console.log((++i%3?"":"Fizz")+(i%5?"":"Buzz")||i)
+
--- js/while.js
+++ js/while.js
@@ -8,12 +8,18 @@ while(i<65536){
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 console.log("I have " + allCones + " cones to sell.");
-// This expression will generate a random number between 1 and 5
-var sold;
+
 do{
-    sold = Math.floor(Math.random() * 5) + 1;
-    if (allCones-sold<0){console.log("Cannot sell you "+sold+" cones I only have "+allCones);continue}
-    allCones-=sold;
-    console.log(sold+" cones sold... " + allCones + " left");
+    // Generate the amount of cones being bought
+    var boughtCones = Math.floor(Math.random() * 5) + 1;
+
+    // Check if you have enough cones left to make the transaction
+    if (allCones-boughtCones<0){
+        console.log("Cannot sell you "+boughtCones+" cones I only have "+allCones);
+        continue;
+    }
+    // If so, continue with the transaction.
+    allCones-=boughtCones;
+    console.log(boughtCones+" cones sold... " + allCones + " left");
 }while(allCones>0);
 console.log("Yay! I sold them all!");
