9819b730b0155c8c3e994719ad4ccd9170c0b94f
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-23 13:29:25 -0500

    Finished mouse events

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,11 +1,22 @@
 "use strict";
 
 $(document).ready(function() {
-
-    var h1 = $('h1');
-    var li = $('li');
-    var p =  $('p');
-    var selectors = $('h1, p, li');
-    li.css('font-size', '20px');
-    alert(selectors.text());
+    $('h1').click(
+        function() {
+            $(this).css('background-color', '#FF0');
+        }
+    );
+    $('p').dblclick(
+        function() {
+            $(this).css('font-size', '18px');
+        }
+    );
+    $('li').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        }
+    );
 });
