eae82c14717c63a2ea28a257a4609beaf3e4a9a0
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-06 14:02:28 -0500

    Uncommented code

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,12 @@
-// const wait = (number) =>{
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve();
-//         },number)
-//     })
-// }
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+const wait = (number) =>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve();
+        },number)
+    })
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 let url="https://api.github.com";
